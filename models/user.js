a189cba9e36bfcd3f4387922262225782528189d
hanin ben slimen <haninebenslimene@gmail.com>
2023-11-23 23:38:37 +0100

    add uniqueValidator plugin

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
         email: { type: String, required: true, unique: true },
@@ -27,9 +28,10 @@ userSchema.set('toJSON', { virtuals: true })
 userSchema.set('toObject', { virtuals: true })
 
 userSchema.methods.toPublic = function() {
-    const user = this.toObject()
-    delete user.password
-    return user
-}
-
+        const user = this.toObject()
+        delete user.password
+        return user
+    }
+    // Utiliser mongoose-unique-validator pour valider l'email
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("User", userSchema)
