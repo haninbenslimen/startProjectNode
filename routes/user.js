d8b66adff68cc438fc3cdb22e875af26df9f3096
hanin ben slimen <haninebenslimene@gmail.com>
2023-11-24 00:58:10 +0100

    update validation on signup route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/user")
+const validation = require("../middlewares/validation")
 
-router.post("/signup", userController.signup)
+router.post("/signup", validation.validateSignup, userController.signup)
 
 router.post("/login", userController.login)
 
